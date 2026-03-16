cddf168a8bd2f373064294e53937d2efc9bda479
sealpp <sealplusplus@outlook.com>
2026-03-17 02:12:50 +0800

    feat(parsers): 添加知乎解析器并支持文章/回答/问题页解析

--- core/parsers/__init__.py
+++ core/parsers/__init__.py
@@ -12,6 +12,7 @@ from .weibo import WeiBoParser
 from .xiaoheihe import XiaoheiheParser
 from .xhs import XHSParser
 from .youtube import YouTubeParser
+from .zhihu import ZhihuParser
 
 __all__ = [
     "BaseParser",
@@ -28,4 +29,5 @@ __all__ = [
     "XiaoheiheParser",
     "XHSParser",
     "YouTubeParser",
+    "ZhihuParser",
 ]
